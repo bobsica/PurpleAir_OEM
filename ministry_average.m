6832ae8e89ed00af41cca20350a88c6977d78d16
Bob Sica <sica@uwo.ca>
2023-02-10 17:04:58 -0500

    code to do weighted ministry average.

--- ministry_average.m
+++
@@ -1,9 +0,0 @@
-% weighted average ministry data
-
-t = readtable('./Example/Ministry.xlsx');
-tarray =  table2array(t(:,2:25));
-time = table2array(t(:,1));
-for i = 1:height(t)
-    hourAv(i) = mean(tarray(i));
-end
-plot(time,hourAv,':o')
