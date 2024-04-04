31dbc77239d5514132d19fb7fc37d49221edf3e7
Bob Sica <sica@uwo.ca>
2024-04-04 16:43:57 -0400

    gitignore problem
    loopDaily writes to PDFs

--- loopDaily.m
+++ loopDaily.m
@@ -4,7 +4,20 @@ RHpAirErr = 0.025;
 iplt = 1;
 for i = 21:24
     pAir_oemF(i,RHpAirErr,iplt)
+    if i == 21
+        exportgraphics(gca, 'dailyWinterStat.pdf');
+    elseif i == 22
+        exportgraphics(gca, 'dailySpringStat.pdf');
+    elseif i == 23
+        exportgraphics(gca, 'dailySummerStat.pdf');
+    else
+        exportgraphics(gca, 'dailyFallStat.pdf');
+    end
 end
+
+
+
+
 
 % plotHGF(RHpAirErr)
 
